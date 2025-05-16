152a9c23c2ec70ea07d68fdae4dd6309f2d82fe9
unknown <rickyjamespanganiban8@gmail.com>
2025-05-16 11:37:33 +0800

    modified

--- lib/ColorPage/sipagong.dart
+++ lib/ColorPage/sipagong.dart
@@ -215,6 +215,13 @@ class _PagongState extends State<Pagong> {
                   height: height * 0.60,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.4),
+                        blurRadius: 5.0,
+                        spreadRadius: 1.0,
+                      ),
+                    ],
                   ),
 
                   child: GestureDetector(
